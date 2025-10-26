85d0e96f71f62854a67cd6724d38a56a4cc2d59a
radhasaik2 <radhasaik.2@gmail.com>
2025-10-26 22:43:52 +0530

    movie-tkt-booking Moviesdto.java

---
+++ Moviez/DTOS/Moviesdto.java
@@ -0,0 +1,82 @@
+package in.react.tktbooking;
+
+public class Moviesdto {
+	private int mid;
+    private String poster;
+    private String title;
+    private String rating;
+    private String language;
+    private String duration;
+    private String genre;
+    private String certification;
+    private String summary;
+    private String cast;
+    private String reviews;
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	public String getPoster() {
+		return poster;
+	}
+	public void setPoster(String poster) {
+		this.poster = poster;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getRating() {
+		return rating;
+	}
+	public void setRating(String rating) {
+		this.rating = rating;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public String getDuration() {
+		return duration;
+	}
+	public void setDuration(String duration) {
+		this.duration = duration;
+	}
+	public String getGenre() {
+		return genre;
+	}
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+	public String getCertification() {
+		return certification;
+	}
+	public void setCertification(String certification) {
+		this.certification = certification;
+	}
+	public String getSummary() {
+		return summary;
+	}
+	public void setSummary(String summary) {
+		this.summary = summary;
+	}
+	public String getCast() {
+		return cast;
+	}
+	public void setCast(String cast) {
+		this.cast = cast;
+	}
+	public String getReviews() {
+		return reviews;
+	}
+	public void setReviews(String reviews) {
+		this.reviews = reviews;
+	}
+	
+}
