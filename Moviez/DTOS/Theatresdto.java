a666b34989b05442bce3300110060a18ab554fef
radhasaik2 <radhasaik.2@gmail.com>
2025-10-26 22:25:21 +0530

    movie-tkt-booking Theatresdto.java

---
+++ Moviez/DTOS/Theatresdto.java
@@ -0,0 +1,47 @@
+package in.react.tktbooking;
+
+public class Theatresdto {
+	private int tid;
+	private String show_day;
+	private String name;
+	private String show_time;
+	private String price;
+	private int mid;
+	public int getTid() {
+		return tid;
+	}
+	public void setTid(int tid) {
+		this.tid = tid;
+	}
+	public String getShow_day() {
+		return show_day;
+	}
+	public void setShow_day(String show_day) {
+		this.show_day = show_day;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getShow_time() {
+		return show_time;
+	}
+	public void setShow_time(String show_time) {
+		this.show_time = show_time;
+	}
+	public String getPrice() {
+		return price;
+	}
+	public void setPrice(String price) {
+		this.price = price;
+	}
+	public int getMid() {
+		return mid;
+	}
+	public void setMid(int mid) {
+		this.mid = mid;
+	}
+	
+}
